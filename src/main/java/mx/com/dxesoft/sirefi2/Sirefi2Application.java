a3167e4eda825788f99cfc81b17de7ca42449e94
Ernesto Mendez <nernesto@gmail.com>
2017-08-27 21:01:04 -0700

    Project initialized

---
+++ src/main/java/mx/com/dxesoft/sirefi2/Sirefi2Application.java
@@ -0,0 +1,12 @@
+package mx.com.dxesoft.sirefi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sirefi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sirefi2Application.class, args);
+	}
+}
